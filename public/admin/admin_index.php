8d6522b10fc15b6d3411a591b55edea7f61e7aeb
figoperdana <192410102022@mail.unej.ac.id>
2022-04-21 01:56:00 +0700

    Update admin_index.php

--- public/admin/admin_index.php
+++ public/admin/admin_index.php
@@ -14,7 +14,7 @@ $results = mysqli_query($conn, $query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment Page</title>
+    <title>Admin Page</title>
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
